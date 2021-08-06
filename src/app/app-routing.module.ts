40d53e9408fbe8a1c427076ff39bfffe5fcebf7a
Fernando <femastro@mgail.com>
2021-08-06 00:00:18 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from '@shared/container/container.component';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./component/pages/home/home.module').then(m => m.HomeModule) }, { path: 'contact', loadChildren: () => import('./component/pages/contact/contact.module').then(m => m.ContactModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContainerComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', loadChildren: () => import('./component/pages/home/home.module').then(m => m.HomeModule) },
+      { path: 'contact', loadChildren: () => import('./component/pages/contact/contact.module').then(m => m.ContactModule) }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
