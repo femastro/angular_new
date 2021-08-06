9cff4a0eb03dd8ca13e39de95ca3acc6ee1f5b9c
Fernando <femastro@mgail.com>
2021-08-05 22:01:06 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from '@shared/container/container.component';
+import { ToolbarComponent } from '@shared/toolbar/toolbar.component';
+
+import { MaterialModule } from "@app/material.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
